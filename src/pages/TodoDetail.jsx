75c0c1dcd4a64ca85077ca46e40cd66ffb531322
Edward Chung <edwardchung2024@gmail.com>
2023-11-17 02:27:17 -0500

    repeattask

--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -1,8 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useTodo } from "../contexts/todoContext";
 import { useState, useEffect } from "react";
-import ProgressBar from "../components/ProgressBar"
-
+import ProgressBar from "../components/ProgressBar";
 
 const TodoDetail = () => {
   const { id } = useParams();
@@ -10,9 +9,10 @@ const TodoDetail = () => {
   const [colorLabel, setColorLabel] = useState("#0d99ff");
   const [progress, setProgress] = useState(0);
   const todo = getTodo(id);
-  const todoDate = todo.date ? new Date(`${todo.date}T${todo.time || '00:00:00'}`) : 0;
+  const todoDate = todo.date
+    ? new Date(`${todo.date}T${todo.time || "00:00:00"}`)
+    : 0;
   const [subtasks, setSubtasks] = useState(todo.subtasks);
-
 
   if (!todo) return <div>No todo found</div>;
 
@@ -21,9 +21,11 @@ const TodoDetail = () => {
   }, [todo]);
 
   useEffect(() => {
-    const totalCheckedSubtasks = subtasks.filter(subtask => subtask.isChecked).length;
-    setProgress(Math.floor((totalCheckedSubtasks / subtasks.length) * 100))
-  }, [subtasks])
+    const totalCheckedSubtasks = subtasks.filter(
+      (subtask) => subtask.isChecked
+    ).length;
+    setProgress(Math.floor((totalCheckedSubtasks / subtasks.length) * 100));
+  }, [subtasks]);
 
   function getLabelForNumber(number) {
     if (number >= 1 && number <= 3) {
@@ -39,52 +41,77 @@ const TodoDetail = () => {
 
   const handleColor = () => {
     const currentDate = new Date();
-    const dayDifference = Math.ceil((todoDate - currentDate) / (1000 * 60 * 60 * 24));
+    const dayDifference = Math.ceil(
+      (todoDate - currentDate) / (1000 * 60 * 60 * 24)
+    );
     if (dayDifference == 0) {
-      setColorLabel("#ff4034")
+      setColorLabel("#ff4034");
+    } else if (dayDifference <= 3 && dayDifference > 0) {
+      setColorLabel("#fe7e08");
+    } else {
+      setColorLabel("#0d99ff");
     }
-    else if (dayDifference <= 3 && dayDifference > 0) {
-      setColorLabel("#fe7e08")
-    }
-    else {
-      setColorLabel("#0d99ff")
-    }
-  }
+  };
 
   const handleSubtask = (index) => {
-    setSubtasks([...subtasks].map((s, i) => (i === index ? {...s, isChecked: !s.isChecked} : s)))
+    // Changes current indexed subtask to !isChecked
+    setSubtasks(
+      [...subtasks].map((s, i) =>
+        i === index ? { ...s, isChecked: !s.isChecked } : s
+      )
+    );
+  };
+
+  const repeatTask = () => {
+    // Changes current indexed subtask to !isChecked
+    setSubtasks([...subtasks].map((s) => ({...s, isChecked: false })));
   }
 
   return (
     <div>
-      <span style={{backgroundColor: colorLabel}}/>
+      <span style={{ backgroundColor: colorLabel }} />
       <p>Task Details</p>
       <p>{todo.todoName}</p>
-      <div>
-        {`Due Date: ${todoDate.toLocaleString('en-US', { timeZone: 'EST'})}`}
-        <br />
-        {`Priority: ${getLabelForNumber(todo.priority)}(${todo.priority}/10)`}
-        <br />
-        {`Complexity: ${getLabelForNumber(todo.complexity)}(${todo.complexity}/10)`}
-        <p>Task Completed: </p>
-        <ProgressBar progress={progress}/>
-        <p>Checklist for subtasks</p>
-        <ul>
-          {subtasks.map((subtask, index) => (
-            <>
-              <div style={{ textDecoration: subtask.isChecked ? "line-through" : "" }}>
-                {subtask.subtask}
-                <button type="button" onClick={(e) => {handleSubtask(index)}}>
-                  Check
-                </button>
-              </div>
-              <br/>
-            </>
-          ))}
-        </ul>
-      </div>
+      {`Due Date: ${todoDate.toLocaleString("en-US", { timeZone: "EST" })}`}
+      <br />
+      {`Priority: ${getLabelForNumber(todo.priority)}(${todo.priority}/10)`}
+      <br />
+      {`Complexity: ${getLabelForNumber(todo.complexity)}(${
+        todo.complexity
+      }/10)`}
+      <p>Task Completed: </p>
+      <ProgressBar progress={progress} />
+      <p>Checklist for subtasks</p>
+      <ul>
+        {subtasks.map((subtask, index) => (
+          <>
+            <div
+              style={{
+                textDecoration: subtask.isChecked ? "line-through" : "",
+              }}
+            >
+              {subtask.subtask}
+              <button
+                type="button"
+                onClick={(e) => {
+                  handleSubtask(index);
+                }}
+              >
+                Check
+              </button>
+            </div>
+            <br />
+          </>
+        ))}
+      </ul>
+      <button
+        type="button"
+        onClick={repeatTask}
+      >
+        Repeat Task
+      </button>
     </div>
   );
-}
+};
 
 export default TodoDetail;
